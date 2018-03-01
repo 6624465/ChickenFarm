80103ef0bb893a31f7dba27576642e4abf9445d2
Prasad SKD <prasad.siripireddi@gmail.com>
2018-03-01 15:38:17 +0530

    commit changes for tcomb installation

--- Screens/Registration/Login.js
+++ Screens/Registration/Login.js
@@ -3,37 +3,27 @@ import {View, Text} from 'react-native';
 
 import {StackNavigator} from 'react-navigation';
 
-export default class Login extends Component{
-    static navigationOptions={
-        title : 'Farm Profile List',
-        headerStyle:{backgroundColor:'#fff'},
-        headerTitleStyle:{color:'#212121'}
-    }
+var t = require('tcomb-form-native');
+var Form = t.form.Form;
+
+var LoginCredential = t.struct({
+    MobileNo: t.Number,             
+    Password: t.String,  
+       
+  });
+
+  export default class Login extends Component{
+    
     render(){
             return(        
-                <View>
-                    <Text>sdfsdf</Text>
-                    </View>        
-            // <Container>
-            //         <Header>
-            //             <Left>
-            //                 <Button transparent onPress={()=>this.props.navigation.navigate('DrawerOpen')}>
-            //                 <Icon ios='ios-menu' android="md-menu" />
-            //                 </Button>
-            //             </Left>
-            //             <Body>
-            //                 <Title>My Farm Dashboard</Title>
-            //             </Body>
-            //         </Header>
-
-            //         <Content contentContainerStyle={{
-            //             flex:1,
-            //             alignItems:'center',
-            //             justifyContent:'center'
-            //         }}>
-            //             <Text>My Farm Dashboard</Text>
-            //         </Content>
-            //     </Container>
+            <View>
+                  <Form
+                  ref='form'
+                  type={LoginCredential}
+                //   options={LoginCredentialOption}
+                  />
+             </View>        
+           
             );
 
     }
