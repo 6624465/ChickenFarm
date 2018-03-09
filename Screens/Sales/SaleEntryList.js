fdcb9cb2596ae2908ebbf38f5994d928a34873f0
Naresh K <naresh@logiconglobal.com>
2018-03-09 15:21:27 +0530

    Sales Changes

--- Screens/Sales/SaleEntryList.js
+++ Screens/Sales/SaleEntryList.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, ActivityIndicator, ListView, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, ActivityIndicator, ListView, TouchableOpacity, Image, TextInput} from 'react-native';
 
-import { StackNavigator } from 'react-navigation';
+import {StackNavigator} from 'react-navigation';
 import { Container, Content, Header, Icon, Left, Title, Body, Button, Footer } from 'native-base';
-
+//import Search from '../Common/Search'
 import api from '../../API/API';
   
 export default class SaleEntryList extends Component{
@@ -11,12 +11,13 @@ export default class SaleEntryList extends Component{
     static navigationOptions={
         drawerLabel: () => null
     }
-
     constructor(props) {
         super(props);
         this.state = {
-          isLoading: true
+          isLoading: true,
+          text: '',
         }
+        this.arrayholder = [] ;
     }
 
     componentDidMount() {
@@ -29,6 +30,7 @@ export default class SaleEntryList extends Component{
                 dataSource: ds.cloneWithRows(responseJson),
             }, function() {
                 // do something with new state
+                this.arrayholder = responseJson ;
             });
             })
             .catch((error) => {
@@ -36,9 +38,21 @@ export default class SaleEntryList extends Component{
         });
     }
 
-    NavigateToDetail=(companycode)=>{            
+    NavigateToDetails=(companycode)=>{            
         this.props.navigation.navigate('SaleEntryDetail');
     }   
+
+    FilterListData=(text)=>{   
+        const newData = this.arrayholder.filter(function(item){
+            const itemData = item.CompanyName.toUpperCase()
+            const textData = text.toUpperCase()
+            return itemData.indexOf(textData) > -1
+        })
+        this.setState({
+            dataSource: this.state.dataSource.cloneWithRows(newData),
+            text: text
+        })
+    }
     
     render(){
         const {navigate}=this.props.navigation;
@@ -54,7 +68,7 @@ export default class SaleEntryList extends Component{
             <Container>
                 <Header>
                     <Left>
-                        <Button transparent onPress={()=>this.props.navigation.navigate('Sales')}>
+                    <Button transparent onPress={()=>this.props.navigation.navigate('Sales')}>
                             <Icon name='arrow-back'/>
                         </Button>
                     </Left>
@@ -63,11 +77,48 @@ export default class SaleEntryList extends Component{
                     </Body>
                 </Header>
 
-                <Content contentContainerStyle={{flex:1, justifyContent:'center'}}>
-                    <ListView
-                        dataSource={this.state.dataSource}
-                        renderRow={(rowData) => <View><TouchableOpacity  onPress={() => this.NavigateToDetail(rowData.CompanyCode)}><Text>{rowData.CompanyCode}, {rowData.CompanyName}</Text></TouchableOpacity></View>}
-                    />
+                <Content>
+                    <View style={styles.container}>
+                        <View style={styles.searchcontainer}> 
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Search..."
+                                value={this.state.text}
+                                onChangeText={(text) => this.FilterListData(text)}
+                                underlineColorAndroid='transparent'
+                            />
+                        </View>
+                        <ListView 
+                            dataSource={this.state.dataSource}
+                            renderRow={(rowData) => 
+                            <View style={styles.listcontainer}>
+                                <TouchableOpacity  onPress={() => this.NavigateToDetails(rowData.CompanyCode)}>
+                                    <View style={{flexDirection:'row' ,flexWrap:'wrap'}} >
+                                        <View style={{width:'20%', alignItems:'center'}}>
+                                            <Image source = { require('../../android/app/src/main/assets/chicken.png') } style={styles.photo}/>                       
+                                        </View>
+                                        <View style={{width:'80%', alignItems:'flex-start'}}>
+                                            <Text style={styles.text}>
+                                                {rowData.CompanyCode}
+                                            </Text>
+                                            <Text style={styles.text}>
+                                                {rowData.CompanyName}
+                                            </Text>
+                                            <Text style={styles.text}>
+                                                {rowData.CompanyName}
+                                            </Text>
+                                            <Text style={styles.text}>
+                                                {rowData.CompanyName}
+                                            </Text>
+                                        </View>
+                                    </View>                            
+                                </TouchableOpacity>
+                            </View>}
+                            renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}
+                            //renderHeader={() => <Search />}
+                            enableEmptySections={true}
+                        />
+                    </View>
                 </Content>
             </Container>
         );
@@ -78,9 +129,46 @@ export default class SaleEntryList extends Component{
 var styles = StyleSheet.create({
     container: {
         flex: 1,
-        //justifyContent: 'center',
+        justifyContent: 'center',
         //marginTop: 120,
-        padding: 20,
-        backgroundColor: '#ffffff',      
-    }
+        padding: 5,
+        backgroundColor: '#C1C1C1',      
+    },
+    searchcontainer: {
+         //flex: 1,
+         padding: 4,
+         flexDirection: 'row',
+         alignItems: 'center',
+         backgroundColor: '#C1C1C1',
+      },
+      input: {
+        height: 40,
+        flex: 1,
+        paddingHorizontal: 20,
+        padding:5,
+        fontSize: 20,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 15,
+      },
+    listcontainer: {
+        flex: 1,
+        padding: 12,
+        flexDirection: 'row',
+        alignItems: 'center',
+      },
+      text: {
+        marginLeft: 12,
+        fontSize: 18,
+        color:'#000'
+      },
+      photo: {
+        height: 70,
+        width: 70,
+        borderRadius: 35,
+      },
+      separator: {
+        flex: 1,
+        height: StyleSheet.hairlineWidth,
+        backgroundColor: '#8E8E8E',
+      },
   });
